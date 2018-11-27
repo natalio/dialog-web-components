33c3cf68bdd0f65443f082a51df8897a8e4ff3f1
Oleg Shilov <me@olegshilov.com>
2018-11-27 21:30:42 +0300

    fix(all): fix all flow errors
    Signed-off-by: Oleg Shilov <me@olegshilov.com>

--- src/components/MessageState/MessageRead.js
+++ src/components/MessageState/MessageRead.js
@@ -4,7 +4,7 @@
  */
 
 import type { PeerInfo } from '@dlghq/dialog-types';
-import React, { Component } from 'react';
+import React, { PureComponent, type Node } from 'react';
 import classNames from 'classnames';
 import { Text } from '@dlghq/react-l10n';
 import { peerToString } from '@dlghq/dialog-types/utils';
@@ -15,8 +15,8 @@ type Props = {
   readBy?: PeerInfo[],
 };
 
-class MessageRead extends Component<Props> {
-  renderAvatars() {
+class MessageRead extends PureComponent<Props> {
+  renderAvatars(): Node {
     const { readBy } = this.props;
     if (!readBy) {
       return null;
