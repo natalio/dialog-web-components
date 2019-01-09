70c132976c86b8d991496d9397a78361e293ae7b
Oleg Shilov <me@olegshilov.com>
2019-01-09 03:03:00 +0300

    feat(Discover): add error and pending states
    render fake cards while pending,
    render pending spinner in title,
    disable create new button while pending

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -8,21 +8,40 @@ import type { Card } from '../DiscoverCard/DiscoverCard';
 import React, { PureComponent, type Node } from 'react';
 import classNames from 'classnames';
 import { Text } from '@dlghq/react-l10n';
-
 import Button from '../Button/Button';
 import DiscoverCard from '../DiscoverCard/DiscoverCard';
+import DiscoverFakeCard from '../DiscoverFakeCard/DiscoverFakeCard';
+import Spinner from '../Spinner/Spinner';
 import styles from './Discover.css';
 
 export type Props = {
   className?: string,
-  items: Card[],
+  pending: boolean,
+  error: string | Error,
+  items: Array<Card>,
   onCreateNew: () => mixed,
   onGoToPeer: (peer: Peer) => mixed,
 };
 
+const MAX_FAKE_CARDS = 4;
+
 class Discover extends PureComponent<Props> {
   renderCards(): Node {
-    const { items } = this.props;
+    const { pending, items } = this.props;
+
+    if (pending) {
+      const cards = [];
+
+      for (let i = 0; i < MAX_FAKE_CARDS; i++) {
+        cards.push(
+          <div className={styles.cardWrapper} key={`card_fake_${i}`}>
+            <DiscoverFakeCard />
+          </div>,
+        );
+      }
+
+      return cards;
+    }
 
     return items.map((card) => {
       return (
@@ -33,14 +52,40 @@ class Discover extends PureComponent<Props> {
     });
   }
 
+  renderError(): Node {
+    const { error } = this.props;
+
+    if (!error) {
+      return null;
+    }
+
+    const errorText = typeof error === 'string' ? error : error.message;
+
+    return (
+      <div className={styles.error}>
+        <Text id={errorText} inline />
+      </div>
+    );
+  }
+
   render() {
+    const { pending } = this.props;
     const className = classNames(styles.container, this.props.className);
 
     return (
       <div className={className}>
         <header className={styles.header}>
           <div className={styles.headerText}>
-            <Text id="Discover.title" tagName="h1" className={styles.title} />
+            <h1 className={styles.title}>
+              <Text id="Discover.title" inline />
+              {pending ? (
+                <Spinner
+                  size="small"
+                  type="dotted"
+                  className={styles.spinner}
+                />
+              ) : null}
+            </h1>
             <Text
               id="Discover.subtitle"
               tagName="h3"
@@ -53,6 +98,7 @@ class Discover extends PureComponent<Props> {
               className={styles.headerButton}
               theme="primary"
               view="outline"
+              disabled={pending}
               onClick={this.props.onCreateNew}
               id="discover_create_new_button"
             >
@@ -60,7 +106,7 @@ class Discover extends PureComponent<Props> {
             </Button>
           </div>
         </header>
-
+        {this.renderError()}
         <div className={styles.cards}>{this.renderCards()}</div>
       </div>
     );
--- src/components/Discover/README.md
+++ src/components/Discover/README.md
@@ -1,32 +1,62 @@
 ```jsx
 const { group, user, channel, bot } = require('../../fixtures/peerInfo');
-const items = [{
-  ...group,
-  description: group.about,
-  members: 23,
-  creator: user.title
-}, {
-  ...channel,
-  description: channel.about,
-  shortname: channel.userName,
-  members: 420,
-}, {
-  ...user,
-  description: user.about,
-  shortname: user.userName
-}, {
-  ...bot,
-  description: bot.about,
-  shortname: bot.userName
-}];
+initialState = {
+  pending: false,
+  error: null,
+  items: [
+    {
+      ...group,
+      description: group.about,
+      members: 23,
+      creator: user.title,
+    },
+    {
+      ...channel,
+      description: channel.about,
+      shortname: channel.userName,
+      members: 420,
+    },
+    {
+      ...user,
+      description: user.about,
+      shortname: user.userName,
+    },
+    {
+      ...bot,
+      description: bot.about,
+      shortname: bot.userName,
+    },
+  ],
+};
 
 const onGoToPeer = (peer) => console.debug(`DISCOVER: go to peer: ${peer.id}`);
 const onCreateNew = () => alert('Open create new modal');
 
+const handlePendingToggle = () => {
+  setState({ pending: !state.pending });
+};
+const handleErrorToggle = () => {
+  setState({
+    error: state.error ? null : 'Something went wrong here! Try again.',
+  });
+};
 
-<Discover
-  items={items}
-  onCreateNew={onCreateNew}
-  onGoToPeer={onGoToPeer}
-/>
+<div>
+  <div className="styleguide__buttons">
+    <Button onClick={handlePendingToggle} theme="warning" size="small">
+      Toggle pending
+    </Button>
+    <Button onClick={handleErrorToggle} theme="danger" size="small">
+      Toggle error
+    </Button>
+  </div>
+  <hr />
+  <Discover
+    error={state.error}
+    pending={state.pending}
+    items={state.items}
+    onCreateNew={onCreateNew}
+    onGoToPeer={onGoToPeer}
+  />
+</div>;
 ```
