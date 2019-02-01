b7cf6749633379b6663fd77b105621901241f641
Andrey Shabanov <a.shabanov@dlg.im>
2019-02-01 13:28:49 +0300

    feat: add style class for callfeedback switcher + disabling send button
    Signed-off-by: Andrey Shabanov <a.shabanov@dlg.im>

--- src/components/CallFeedback/CallFeedback.js
+++ src/components/CallFeedback/CallFeedback.js
@@ -79,6 +79,7 @@ class CallFeedback extends PureComponent<Props, State> {
 
   render() {
     const className = classNames(styles.container, this.props.className);
+    const { text, rating } = this.state;
 
     return (
       <HotKeys onHotKey={this.handleHotkey}>
@@ -114,11 +115,13 @@ class CallFeedback extends PureComponent<Props, State> {
                 value={this.state.addLogs}
                 onChange={this.handleAddLogsToggle}
                 label="CallFeedback.add_logs"
+                className={styles.switcher}
               />
             </ModalBody>
             <ModalFooter className={styles.footer}>
               <Button
                 wide
+                disabled={text !== '' && rating !== 0}
                 type="submit"
                 theme="success"
                 rounded={false}
